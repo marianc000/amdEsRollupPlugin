72489181d6ff5ccddb68c93870e6ad6d190a0cd9
mcaikovs <marian.caikovski@gmail.com>
2021-04-25 18:27:04 +0200

    works

--- plugin.js
+++ plugin.js
@@ -1,7 +1,15 @@
 const path = require('path');
+const { normalizePath } = require('@rollup/pluginutils');
+const { readFileSync } = require('fs');
+
 const workingDir = process.cwd();
- 
-function ImportmapPlugin({ imports} = {imports:[]}) {
+
+function toAbsolutePath(relativePath){
+    return  path.resolve(workingDir, relativePath);
+}
+//await loadHTML('./ONGOING.html',import.meta.url);
+
+function ImportmapPlugin({ imports } = { imports: [] }) {
     let cache;
 
     console.log("XXX esmImportToUrl", workingDir, imports);
@@ -15,32 +23,51 @@ function ImportmapPlugin({ imports} = {imports:[]}) {
         },
 
         resolveId(source, importer) {
-            console.log("XXX resolveId", source, importer);
+            // console.log("XXX resolveId", source, importer);
             const url = cache.get(source);
             if (url) {
-                return url;
+                return toAbsolutePath(url);
             } else {
                 const entry = Array.from(cache).find(([key, val]) => source.startsWith(key)
                     && source.length > key.length);
                 if (entry) {
                     const [key, val] = entry;
-                    const r = source.replace(key, val);
-                    console.log("found", key, val, source, '=>', r);
+                    const r = toAbsolutePath(source.replace(key, val));
+                  //  console.log("found", key, val, source, '=>', r);
                     return r;
                 }
             }
             return null;
         },
 
+        // load (id){
+        //     if (id.includes('dataHolder'))
+        //      console.log("---------------------",id);
+        //    // else console.log("---",id);
+        //     return null;
+        // },
+
         resolveImportMeta(property, obj) {
-         
-            // console.log("XXX resolveImportMeta",property, obj,pathN.relative(workingDir, obj.moduleId));
+
+            // console.log("XXX resolveImportMeta",property, obj,normalizePath(athN.relative(workingDir, obj.moduleId)));
             return null;
         },
 
         transform(code, id) {
-            // console.log("XXX transform",id, code.length);
-            return code;
+            const re = /await +loadHTML\('([^,]+)',/g;
+            function replacer(match, p1, offset, string) {
+                console.log(p1);
+                const filePath = path.resolve(path.dirname(id), p1);
+                console.log("transform found ", p1, id, filePath);
+                const html = readFileSync(filePath,{encoding  : 'utf8'});
+
+                return JSON.stringify(html) + "; //";
+
+            }
+            const newCode = code.replace(re, replacer);
+            if (code.length != newCode.length)
+                console.log("transform ", code.length, '=>', newCode.length);
+            return newCode;
         }
 
     };
